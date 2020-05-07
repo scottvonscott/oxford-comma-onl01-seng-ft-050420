4aa34d26a94a2b099d04cbebf17ab8eee5b1aa02
Scott  Keplinger <skeplin17@gmail.com>
2020-05-07 03:37:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,8 @@ def oxford_comma(array)
   else array.length > 2
     new_array1 = []
     new_array2 = []
-    new_array1 << array[0..array.size-2]
-    new_array2 << array.size[-1]
+    new_array1 << array[0..array.size-1]
+    new_array2 << array.last
     new_array1.join(", ") + new_array2.join(", and ")
   end
 end
