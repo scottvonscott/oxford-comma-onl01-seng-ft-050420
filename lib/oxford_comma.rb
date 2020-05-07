aacf21b59ae84bba0b93004f873bb353805e3ce5
Scott  Keplinger <skeplin17@gmail.com>
2020-05-07 03:27:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
       array.join(" and ")
   else array.length > 2
-      array[0,1].join(, ) + array[-1].join(", and ")
+      array[0,1].join(", ") + array[-1].join(", and ")
   end
 end
 
